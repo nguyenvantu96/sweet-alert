806671d236c99215c3166963a91760290b87fa60
nguyenvantu96 <36929721+nguyenvantu96@users.noreply.github.com>
2019-03-26 11:41:45 +0700

    index.js
    index

---
+++ index.js
@@ -0,0 +1,5 @@
+import { SweetAlert } from 'sweetalert/typings/core';
+import * as _swal from 'sweetalert';
+const swal: SweetAlert = _swal as any;
+
+ swal('Thông báo', 'Hãy chọn file đính kèm', 'warning');
